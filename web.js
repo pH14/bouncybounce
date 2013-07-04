726d272a1fa5f2016b06a54fed1031a98781eded
Paul Hemberger <pH14@mit.edu>
2013-07-04 15:31:05 -0700

    adding ejs dependency

--- web.js
+++ web.js
@@ -1,10 +1,15 @@
 var express = require("express");
 var app = express();
+
+app.set('views', __dirname + '/views');
+app.engine('html', require('ejs').renderFile);
+
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-    response.send('Hello World!');
+    response.render('index.html');
 });
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
